9f4bf2b22c03d75b54fe6c23d9ca44cdcc9b6ee9
sandoval <sandoval@DESKTOP-25NI4RD>
2017-04-03 12:22:47 -0500

    dos scripts mas...

---
+++ ExtJs/ExtJs/js/fwctrl/c07_eventos.js
@@ -0,0 +1,177 @@
+﻿/* 
+ * Programa: fwctrl/c07_eventos.js
+ * Objetivo: Crear un "controlador" de eventos universal
+ * 
+ */
+Ext.onReady(function () {
+
+    /**
+     * La idea general es crear un objeto "listeners" el cual atrape
+     * los eventos de todos los controles/campos,  asi  concentramos
+     * el código en un solo objeto.
+     * 
+     * En este caso el objeto para  atrapar  los  eventos  se  llama
+     * "evtctrl".
+     * 
+     * Para distinguir cual es el objeto que esta generando el evento
+     * podemos asignarle una propiedad a todos los  objetos (en  este
+     * caso le puse "evt"), tambien podríamos usar la  propiedad "id"
+     * que ya existe para todos los objetos.
+     * 
+     * Si queremos saber a cual objeto corresponde  un  evento,  solo 
+     * hay que usar esta propiedad.
+     * 
+     * Ahora ya teniendo este objeto definido, solo tenemos  que usar
+     * la propiedad "listeners" de cada objeto para asignarle eventos
+     * 
+     */
+    var evtctrl = {
+        keyup: function (obj, key) {
+            console.log(obj.evt + ':key');
+        },
+
+        focus: function (obj, t, e) {
+            console.log(obj.evt + ':focus');
+        },
+
+        change: function (obj, newValue, oldValue, eOpts) {
+            console.log(obj.evt + ':change');
+            console.log(oldValue);
+            console.log(newValue);
+        }
+    };
+
+
+
+    var fd = new Ext.form.field.Display({
+        fieldLabel: 'Nombre',
+        anchor: '90%',
+        value: 'Ni Nombre',
+    });
+
+    var ft = new Ext.form.field.Text({
+        evt: 'my.text',
+        fieldLabel: 'Puesto',
+        emptyText: 'Captura aqui tu puesto',
+        anchor: '90%',
+        disabled: false,
+        enableKeyEvents: true,
+        listeners: evtctrl
+    });
+
+    var fta = new Ext.form.field.TextArea({
+        evt: 'my.textArea',
+        fieldLabel: 'Comentarios',
+        emptyText: 'Captura aqui tu comentario',
+        anchor: '90%',
+        disabled: false,
+        listeners: evtctrl
+    });
+
+    var chk = new Ext.form.field.Checkbox({
+        evt: 'my.checkBox',
+        fieldLabel: 'X-tuti',
+        boxLabel: 'Programador',
+        checked: true,
+        listeners: evtctrl
+    });
+
+    var fdt = new Ext.form.field.Date({
+        fieldLabel: 'Nacimiento',
+        value: new Date(),
+        maxValue: new Date(2017, 12, 31),
+        minValue: new Date(),
+    });
+
+    var fn = new Ext.form.field.Number({
+        fieldLabel: 'Edad',
+        value: 40,
+        maxValue: 99,
+        minValue: 0
+    });
+
+
+    var ftime = new Ext.form.field.Time({
+        fieldLabel: 'Hora',
+        minValue: '6:00 AM',
+        maxValue: '8:00 PM',
+        increment: 30
+    });
+
+    var rg = new Ext.form.RadioGroup({
+        fieldLabel: 'Languaje preferido',
+        columns: 2,
+
+        items: [
+            { boxLabel: 'VFP', name: 'lenguaje', inputValue: 'VFP' },
+            { boxLabel: 'Visual Basic', name: 'lenguaje', inputValue: 'VB' },
+            { boxLabel: 'C#', name: 'lenguaje', inputValue: 'CS' },
+            { boxLabel: 'Javascript', name: 'lenguaje', inputValue: 'js', checked: true },
+            { boxLabel: 'PHP', name: 'lenguaje', inputValue: 'php' }
+        ],
+    });
+
+    var cb = new Ext.form.ComboBox({
+        fieldLabel: 'Ciudad',
+        store: ['Monclova', 'San Buena', 'Castaños'],
+        forceSelection: true,
+        emptyText: 'Selecciona una ciudad...',
+        editable: false,
+    });
+
+    var chkg = new Ext.form.CheckboxGroup({
+        fieldLabel: 'Lenguajes de programación',
+        columns: 2,
+
+        items: [
+            { boxLabel: 'VFP', name: 'lenguaje', inputValue: 'VFP' },
+            { boxLabel: 'Visual Basic', name: 'lenguaje', inputValue: 'VB' },
+            { boxLabel: 'C#', name: 'lenguaje', inputValue: 'CS' },
+            { boxLabel: 'Javascript', name: 'lenguaje', inputValue: 'js' },
+            { boxLabel: 'PHP', name: 'lenguaje', inputValue: 'php' }
+        ]
+    });
+
+    var fldset = new Ext.form.FieldSet({
+        title: 'Agrupados',
+        collapsed: false,
+        collapsible: true,
+        layout: 'anchor',
+        width: '70%',
+        margin: '5 5 5 5',
+        defaults: { xtype: 'textfield' },
+
+        items: [
+            { fieldLabel: 'Nombre:', value: 'Juan Antonio' },
+            { fieldLabel: 'e-mail:' }
+        ],
+    });
+
+
+    var myForm = new Ext.FormPanel({
+        defaults: { xtype: 'textfield' },
+        bodyStyle: 'padding: 10px',
+        layout: 'anchor',
+
+        items: [
+            fd,
+            ft,
+            fta,
+            chk,
+            fdt,
+            fn,
+            ftime,
+            rg,
+            cb,
+            chkg,
+            fldset
+        ]
+    });
+
+    var win = new Ext.Window({
+        title: 'Mi Form en una Ventana',
+        width: 500,
+        items: myForm
+    });
+    win.show();
+});
---
+++ ExtJs/ExtJs/js/fwctrl/c08_store.js
@@ -0,0 +1,123 @@
+﻿/* 
+ * Programa: fwctrl/c08_store.js
+ * Objetivo: store
+ *           Este objeto administra el manejo de datos, inclusive
+ *           permite obtener los datos desde un a fuente externa,
+ *           como puede ser un servidor (ASP, PHP, JSON, etc.).
+ */
+Ext.onReady(function () {
+
+    var st = new Ext.data.Store({
+        // DEBE ponerse el nombre de los campos
+        fields: ['firstName', 'lastName'],
+
+        /**
+         * La principal propiedad del objeto store es 'data'
+         * que es donde guardamos los datos.
+         * Esta puede almacenar cualquier dato, lo normal es
+         * que contenga un array de registros (objetos).
+         * 
+         */
+        data: [
+            { firstName: 'Ed',    lastName: 'Spencer' },
+            { firstName: 'Tommy', lastName: 'Maintz' },
+            { firstName: 'Aaron', lastName: 'Conran' },
+            { firstName: 'Jamie', lastName: 'Avins' }
+        ]
+    });
+
+
+    /**
+     * Y asi podemos tener acceso a los registros individuales :)
+     * 
+     * NOTA: Hay que consultar en la pagina de ayuda de extjs para
+     *       ver todas las propiedades y metodos de est objeto.
+     */
+    console.log("Registros: " + st.getTotalCount());
+    console.log(st.data.items[0].data.firstName);
+    var rec = st.getAt(1).data;
+    console.log("Nombre: " + rec.firstName);
+
+
+
+    /* Ahora lo cargamos desde un JSON en un URL externo
+     * ==== ESTO REQUIERE QUE SE LLAME A ESTA PAGINA 
+     *                     DESDE UN SERVIDOR WEB !!! ===
+     */
+    var stJson = new Ext.data.Store({
+        fields: ['Nombre', 'Edad'],
+        actionMethods: { read: "POST" },
+        autoLoad: true,
+
+        proxy: {
+            type: 'ajax',
+            url: './resources/json/ejemplo_nombres.json',
+
+            reader: {
+                type: 'json',
+                root: 'data'
+            }
+        },
+    });
+
+    // Porque la siguiente linea no muestra los datos?... si tiene el autoLoad:true ???
+    try {
+        console.log(stJson.getAt(1).data.Nombre);
+    } catch (err) {
+        console.log(err.message)
+    }
+
+
+
+    /* 
+     * Y porque, cargandolo de esta manera (abajo), si muestra los datos???
+     * 
+     * Pues es parte de la filosofia "asincrona" de javascript!, esto es:
+     * Arriba, el autoload hace que se carguen automaticamente los datos
+     * desde la pagina, pero (por esta forma de trabajar de js) la PC ---
+     * sigue ejecutando el código,  por lo que al ejecutar la linea donde
+     * intenta mostrar el campo Nombre del registro 1 el servidor todavia
+     * no regresa los datos.
+     * 
+     * Al contrario de este código (abajo). Aqui se le indica que cuando-
+     * reciba los datos del servidor ejecuta la función "callback".
+     * Asi, dentro de esta función ya podemos tranquilamente usar los ---
+     * datos regresados (siempre consultando la variable success).
+     * 
+     * Por lo que es recomendable dejar "autoLoad:false" y usar el metodo
+     * "load" del store. Asi podemos enviarle un mensaje al usuario------
+     * cuando por alguna razon no se tenga acceso a los datos.
+     * 
+     */
+    stJson.load({
+        callback: function (records, operation, success) {
+            if (!success) {
+                Ext.alert("Error en la jugada!");
+                return;
+            }
+
+            if (success) {
+                console.log("Ya se cargo");
+                console.log(records[0].data.Nombre);  // desde el parametro
+                console.log(stJson.getAt(0).data.Nombre);  // con el store
+            }
+        }
+    });
+
+
+
+    // OTRAS FUNCIONES INTERESANTES
+    st.sort({
+        property: 'firstName',
+        direction: 'DESC'
+    });
+    console.log("Ordenada desc x nombre: " + st.getAt(0).data.firstName);
+
+    st.clearFilter();
+    st.filter({
+        property: 'fistName',
+        value: 'Yo',
+        anyMatch: true,
+        caseSensitive: false
+    });
+});
